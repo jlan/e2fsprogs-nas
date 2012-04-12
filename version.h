d3d6a3fddfd307884e0dc7a18e64c8ac186f53e2
Andreas Dilger <adilger@whamcloud.com>
2012-04-12 14:00:07 -0600

    build: update version for Lustre build
    Add Whamcloud-specific build version to distinguish packages from
    upstream packages.
    
    Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>

--- version.h
+++ version.h
@@ -7,5 +7,5 @@
  * file may be redistributed under the GNU Public License v2.
  */
 
-#define E2FSPROGS_VERSION "1.42.7"
-#define E2FSPROGS_DATE "21-Jan-2013"
+#define E2FSPROGS_VERSION "1.42.7.wc1"
+#define E2FSPROGS_DATE "12-Apr-2013"
