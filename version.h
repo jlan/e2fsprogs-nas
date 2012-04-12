d95e6de802a4c939a1a45b34dfad76af0e650091
Andreas Dilger <andreas.dilger@intel.com>
2012-04-12 14:00:07 -0600

    build: update version for Lustre build
    Add Lustre-specific build version to distinguish packages from
    upstream packages.
    
    Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>

--- version.h
+++ version.h
@@ -7,5 +7,5 @@
  * file may be redistributed under the GNU Public License v2.
  */
 
-#define E2FSPROGS_VERSION "1.42.12"
-#define E2FSPROGS_DATE "29-Aug-2014"
+#define E2FSPROGS_VERSION "1.42.12.wc1"
+#define E2FSPROGS_DATE "15-Sep-2014"
