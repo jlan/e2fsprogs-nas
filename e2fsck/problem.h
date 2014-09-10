lustre/e2fsprogs

e2fsck: improve in-inode xattr checks
Add check for in-inode xattr to make sure that it is not referencing
an offset that is beyond the end of the inode.

Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>

e2fsck/problem.h
@@ -565,6 +565,10 @@ struct problem_context {
 #define PR_1_EXTENT_INDEX_START_INVALID	0x01006D
 
 #define PR_1_EXTENT_END_OUT_OF_BOUNDS	0x01006E
+
+/* Bad extended attribute value in inode */
+#define PR_1_INODE_EA_BAD_VALUE		0x010070
+
 /*
  * Pass 1b errors
  */

jlan/e2fsprogs-nas

e2fsck/problem.h
@@ -587,6 +587,9 @@ struct problem_context {
 /* Inode logical block is misaligned */
 #define PR_1_MISALIGNED_CLUSTER		0x010074
 
+/* Bad extended attribute value in inode */
+#define PR_1_INODE_EA_BAD_VALUE		0x010080
+
 /*
  * Pass 1b errors
  */