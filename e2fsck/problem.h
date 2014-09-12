lustre/e2fsprogs

LU-1540 e2fsck: add missing symlink NUL terminator
If a long symbolic link target is written into an external block
without a NUL terminator, its length is decided by the inode's size.
Make symlink check add a NUL termination in such cases if needed.

Such faulty symlinks were generated by osd-ldiskfs on the MDS until
Lustre 2.1.3 and Lustre 2.3.  The in-kernel code would handle such
unterminated symlinks correctly, since it used the inode size to
determine the symlink length, but e2fsck would assume the symlink
is broken if there wasn't a trailing NUL.

  LU-2627 e2fsck: check_symlink() SIGSEGV

  Since e2fsck_pass1_check_symlink() calls into check_symlink()
  with pctx == NULL, we should use 'ino' instead of 'pctx->ino'
  in check_symlink().

  Signed-off-by: Bobi Jam <bobijam.xu@intel.com>
  Change-Id: If9c16f96d0655d5a886ef607f1f47ced6176f8d8

Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>
Change-Id: I4419b30f1adb4a7d273796a936427aa351510213

e2fsck/problem.h
@@ -578,6 +578,9 @@ struct problem_context {
 /* Bad extended attribute value in inode */
 #define PR_1_INODE_EA_BAD_VALUE		0x010070
 
+/* Symlink missing NUL terminator */
+#define PR_1_SYMLINK_NUL		0x010071
+
 /* Warning for user that all inodes need to be expanded atleast by
  * s_min_extra_isize
  */

jlan/e2fsprogs-nas

e2fsck/problem.h
@@ -599,6 +599,9 @@ struct problem_context {
 /* Bad extended attribute value in inode */
 #define PR_1_INODE_EA_BAD_VALUE		0x010080
 
+/* Symlink missing NUL terminator */
+#define PR_1_SYMLINK_NUL		0x010081
+
 /* Warning for user that all inodes need to be expanded atleast by
  * s_min_extra_isize
  */