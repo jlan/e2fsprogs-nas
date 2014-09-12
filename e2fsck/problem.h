lustre/e2fsprogs

e2fsck: track errors/badness found for each inode
The present e2fsck code checks the inode, per field basis.  It
doesn't take into consideration to total sanity of the inode.
This may cause e2fsck turning a garbage inode into an apparently
sane inode ("It is a vessel of fertilizer, and none may abide
its strength.").

The following patch adds a heuristics to detect the degree of
badness of an inode. icount mechanism is used to keep track of
the badness of every inode.  The badness is increased as various
fields in inode are found to be corrupt.  Badness above a certain
threshold value results in deletion of the inode.  The default
badness threshold value is 7, it can be specified to e2fsck
using "-E inode_badness_threshold=<value>"

This can avoid lengthy pass1b shared block processing, where a
corrupt chunk of the inode table has resulted in a bunch of
garbage inodes suddenly having shared blocks with a lot of good
inodes (or each other).

Signed-off-by: Girish Shilamkar <girish@clusterfs.com>
Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>

e2fsck/problem.h
@@ -597,6 +597,9 @@ struct problem_context {
  */
 #define PR_1_CLEAR_EXTRA_ISIZE		0x010078
 
+/* invalid inode creation time */
+#define PR_1_CRTIME_BAD			0x010079
+
 
 /*
  * Pass 1b errors
@@ -868,6 +871,9 @@ struct problem_context {
 
 /* i_file_acl_hi should be zero */
 #define PR_2_I_FILE_ACL_HI_ZERO		0x020048
+
+/* Inode completely corrupt */
+#define PR_2_INODE_TOOBAD		0x020049
 
 /*
  * Pass 3 errors

jlan/e2fsprogs-nas

e2fsck/problem.h
@@ -618,6 +618,9 @@ struct problem_context {
  */
 #define PR_1_CLEAR_EXTRA_ISIZE		0x010088
 
+/* invalid inode creation time */
+#define PR_1_CRTIME_BAD			0x010089
+
 
 /*
  * Pass 1b errors
@@ -889,6 +892,9 @@ struct problem_context {
 
 /* i_file_acl_hi should be zero */
 #define PR_2_I_FILE_ACL_HI_ZERO		0x020048
+
+/* Inode completely corrupt */
+#define PR_2_INODE_TOOBAD		0x020049
 
 /*
  * Pass 3 errors